ee857d5a78ca141b6832af2f433ba9b5a6968cb6
PierricM380 <pierric.marye@wanadoo.fr>
2022-03-18 17:02:40 +0100

    Réalisation de l'exercice sur la gestion des états et le passage des propriétés

---
+++ live_swiftUI_numero1/Live 3/PierricView.swift
@@ -0,0 +1,63 @@
+//
+//  PierricView.swift
+//  live_swiftUI_numero1
+//
+//  Created by Pierric Marye on 18/03/2022.
+//
+
+import SwiftUI
+
+struct PierricView: View {
+    
+    // propriété simple sans expansion dans l'application
+    var colorSwift = Color("Color3")
+    
+    /* définition de propriétés "property wrapper" d'états pour les vues Picker et Slider */
+    // Arrière plan
+    @State var CouleurArrierePlan = Color.accentColor
+    // Opacité
+    @State var selectedOpacity: Double = 1.0
+    
+    var body: some View {
+        ZStack {
+            CouleurArrierePlan.opacity(selectedOpacity).ignoresSafeArea()
+            VStack(spacing: 100.0) {
+                
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200.0, height: 200.0)
+                    .foregroundColor(colorSwift)
+                    .shadow(color: .black, radius: 10, x: 5, y: 5)
+                
+                Spacer()
+                
+                VStack(spacing: 25.0) {
+                    
+                    Spacer()
+                    // Vue ColorPicker possédant des paramètres: texte à afficher et Binding
+                    ColorPicker("Choisir une couleur de fond:", selection: $CouleurArrierePlan)
+                    
+                    // Vue ColorPicker possédant des paramètres: Binding et le range (étalonnage)
+                    Slider(value: $selectedOpacity, in: 0...1)
+                    // affichage de la valeur en % grâce au casting. Pas besoin de $ car sera uniquement pour cette vue
+                    Text("Opacité: \((selectedOpacity * 100), specifier: "%.f") %")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                }
+                .padding()
+                .frame(width: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/)
+                .background(.ultraThinMaterial)
+                .cornerRadius(15)
+                .shadow(color: .black.opacity(0.5), radius: 15, x: 0, y: 0)
+            }
+            .padding()
+        }
+    }
+}
+
+struct PierricView_Previews: PreviewProvider {
+    static var previews: some View {
+        PierricView()
+    }
+}
