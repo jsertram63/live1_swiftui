b5537cf4f566b2743f17faa5ab94ebcde36860eb
jsertram63 <jser>
2022-03-18 12:18:49 +0100

    Ajout de la tabac

--- live_swiftUI_numero1/ContentView.swift
+++ live_swiftUI_numero1/ContentView.swift
@@ -20,67 +20,24 @@ struct ContentView: View {
     // le binding
     //@Binding
     
+    // initialisation du constructeur
+    init() {
+        // stylisation de la tabbar
+        UITabBar.appearance().backgroundColor = UIColor.lightGray
+    }
+    
     var body: some View {
-        VStack(spacing: 25.0) {
-            // Principe du State
-            VStack {
-                Text("Changement d'état avec @State")
-                    .foregroundColor(textColor)
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                
-                // vue container qui permet d'aligner horizontalement des containers
-                HStack(alignment: .center) {
-                    // Spacer container qui va permettre d'occuper l'espace libre (homogénisation de l'espace)
-                    Spacer()
-                    
-                    Button {
-                        
-                        self.imageFill.toggle()
-                    } label: {
-                        Text("Changer d'image")
-                            .padding(10.0)
-                    }
-                    .foregroundColor(Color.white)
-                    .background(.blue)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    
-                    Spacer()
-                    
-                    //  container Image affiche une image
-                    // sun.haze ou sun.max.fill : sfsymbol (fourni appel pour travailer)
-                    Image(systemName: imageFill ? "sun.haze" : "sun.max.fill")
-                        .resizable()
-                        // .frame permet de spécifier la taille de l'image.
-                        .frame(width: 75.0, height: 75.0)
-                        .foregroundColor(imageFill ? .orange : .yellow)
-                    
-                    Spacer()
+        TabView {
+            StateBinding()
+                .tabItem {
+                    Image(systemName: "link")
+                    Text("Live 1-2")
                 }
-            }
-            .padding(20.0)
+            live3View()
+                .tabItem {
+                    Text("Live 3-4")
+                }
             
-            Divider()
-            // Pricipe du binding
-            // Vstack (vue container pour empiler les éléments les uns en dessous des autres)
-            VStack {
-                Text("Changement de couleur avec le @Binding")
-                    // .modifier instance qui permette de modifier de styliser le text
-                    .foregroundColor(textColor)
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                
-                HStack {
-                    // liaison avec le boutton choix de couleur
-                    // la liason est symbolisé le $
-                    SousVue(viewColor: .red, selectedColor: $textColor)
-                    SousVue(viewColor: .blue, selectedColor: $textColor)
-                    SousVue(viewColor: .yellow, selectedColor: $textColor)
-                    SousVue(viewColor: .green, selectedColor: $textColor)
-                }
-            }
-            .padding(20.0)
         }
     }
 }
---
+++ live_swiftUI_numero1/live3View.swift
@@ -0,0 +1,20 @@
+//
+//  live3View.swift
+//  live_swiftUI_numero1
+//
+//  Created by Lunack on 18/03/2022.
+//
+
+import SwiftUI
+
+struct live3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct live3View_Previews: PreviewProvider {
+    static var previews: some View {
+        live3View()
+    }
+}
