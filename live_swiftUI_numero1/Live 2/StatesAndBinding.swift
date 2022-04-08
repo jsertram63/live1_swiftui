e9380e06197c6d6e917b1546d9e413d9216d37cc
PierricM380 <pierric.marye@wanadoo.fr>
2022-04-08 11:39:54 +0200

    Ajout d'une ScrollView pour compenser l'affichage sur petits écran

--- live_swiftUI_numero1/Live 2/StatesAndBinding.swift
+++ live_swiftUI_numero1/Live 2/StatesAndBinding.swift
@@ -17,100 +17,102 @@ struct StatesAndBinding: View {
     
     var body: some View {
         ZStack {
-            Color("Color1").ignoresSafeArea()
+            Color("Color1").ignoresSafeArea(edges: .bottom)
             
-            VStack(alignment: .center, spacing: 25.0) {
-                VStack(spacing: 25.0) {
-                    Text("Gestion des états et le passage de données")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                    
-                    Text(text)
-                }
-                
-                Spacer()
-                
-                Divider()
-                    .background(.black)
-                
-                Spacer()
-                
-                // Principe du State
-                VStack(spacing: 25.0) {
-                    Text("Changement d'état avec @State")
-                        .font(.title3)
-                        .fontWeight(.medium)
-                    
-                    // vue container qui permet d'aligner horizontalement des containers
-                    HStack(alignment: .center) {
-                        // Spacer container qui va permettre d'occuper l'espace libre (homogénisation de l'espace)
-                        Spacer()
+            ScrollView {
+                VStack(alignment: .center, spacing: 25.0) {
+                    VStack(spacing: 25.0) {
+                        Text("Gestion des états et le passage de données")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
                         
-                        Button {
-                            imageFill.toggle()
-                        } label: {
-                            Text("Changer d'image")
-                                .padding(10.0)
+                        Text(text)
+                    }
+                    
+                    Spacer()
+                    
+                    Divider()
+                        .background(.black)
+                    
+                    Spacer()
+                    
+                    // Principe du State
+                    VStack(spacing: 25.0) {
+                        Text("Changement d'état avec @State")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                        
+                        // vue container qui permet d'aligner horizontalement des containers
+                        HStack(alignment: .center) {
+                            // Spacer container qui va permettre d'occuper l'espace libre (homogénisation de l'espace)
+                            Spacer()
+                            
+                            Button {
+                                imageFill.toggle()
+                            } label: {
+                                Text("Changer d'image")
+                                    .padding(10.0)
+                            }
+                            .foregroundColor(Color.white)
+                            .background(Color("Color2"))
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            
+                            Spacer()
+                            
+                            //  container Image affiche une image
+                            // sun.haze ou sun.max.fill : sfsymbol (fourni appel pour travailer)
+                            Image(systemName: imageFill ? "sun.haze" : "sun.max.fill")
+                                .resizable()
+                                // .frame permet de spécifier la taille de l'image.
+                                .frame(width: 75.0, height: 75.0)
+                                .foregroundColor(imageFill ? Color("Color3") : Color.accentColor)
+                            
+                            Spacer()
                         }
-                        .foregroundColor(Color.white)
-                        .background(Color("Color2"))
-                        .cornerRadius(10)
-                        .shadow(radius: 10)
+                    }
+                    
+                    Spacer()
+                    
+                    Divider()
+                        .background(.black)
+                    
+                    Spacer()
+                    // Pricipe du Binding
+                    // Vstack (vue container pour empiler les éléments les uns en dessous des autres)
+                    VStack(spacing: 25.0) {
+                        Text("Changement de couleur avec le @Binding")
+                            // .modifier instance qui permette de modifier de styliser le text
+                            .font(.title3)
+                            .fontWeight(.medium)
                         
-                        Spacer()
+                        Text("Cliquez sur les pastilles")
+                            .fontWeight(.bold)
+                            .foregroundColor(textColor)
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(15)
                         
-                        //  container Image affiche une image
-                        // sun.haze ou sun.max.fill : sfsymbol (fourni appel pour travailer)
-                        Image(systemName: imageFill ? "sun.haze" : "sun.max.fill")
-                            .resizable()
-                            // .frame permet de spécifier la taille de l'image.
-                            .frame(width: 75.0, height: 75.0)
-                            .foregroundColor(imageFill ? Color("Color3") : Color.accentColor)
-                        
-                        Spacer()
+                        HStack {
+                            // liaison avec le boutton choix de couleur
+                            // la liason est symbolisé le $
+                            Spacer()
+                            SousVue(viewColor: .red, selectedColor: $textColor)
+                            Spacer()
+                            SousVue(viewColor: Color("Color2"), selectedColor: $textColor)
+                            Spacer()
+                            SousVue(viewColor: Color.accentColor, selectedColor: $textColor)
+                            Spacer()
+                            SousVue(viewColor: Color("Color1"), selectedColor: $textColor)
+                            Spacer()
+                        }
                     }
                 }
-                
-                Spacer()
-                
-                Divider()
-                    .background(.black)
-                
-                Spacer()
-                // Pricipe du Binding
-                // Vstack (vue container pour empiler les éléments les uns en dessous des autres)
-                VStack(spacing: 25.0) {
-                    Text("Changement de couleur avec le @Binding")
-                        // .modifier instance qui permette de modifier de styliser le text
-                        .font(.title3)
-                        .fontWeight(.medium)
-                    
-                    Text("Cliquez sur les pastilles")
-                        .fontWeight(.bold)
-                        .foregroundColor(textColor)
-                        .padding()
-                        .background(.ultraThinMaterial)
-                        .cornerRadius(15)
-                    
-                    HStack {
-                        // liaison avec le boutton choix de couleur
-                        // la liason est symbolisé le $
-                        Spacer()
-                        SousVue(viewColor: .red, selectedColor: $textColor)
-                        Spacer()
-                        SousVue(viewColor: Color("Color2"), selectedColor: $textColor)
-                        Spacer()
-                        SousVue(viewColor: Color.accentColor, selectedColor: $textColor)
-                        Spacer()
-                        SousVue(viewColor: Color("Color1"), selectedColor: $textColor)
-                        Spacer()
-                    }
-                }
-            }
-            .foregroundColor(.white)
-            .multilineTextAlignment(.center)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
             .padding()
+            }
         }
         
     }
